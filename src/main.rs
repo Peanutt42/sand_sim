16ee6aea88935d3b9cf272e78dbd4c02a0403568
Peanutt42 <p3anutt42@gmail.com>
2024-01-25 12:04:30 +0100

    Made window resizable
    (simualtion size will not change)

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ fn main() {
         "Sand simulation",
         WIDTH, HEIGHT,
         WindowOptions {
-            resize: false,
+            resize: true,
             ..WindowOptions::default()
         })
         .unwrap_or_else(|e| panic!("Failed to create window: {e}"));
@@ -35,27 +35,17 @@ fn main() {
             }
         }
 
-        if window.get_mouse_down(MouseButton::Left) {
+        let mouse_left = window.get_mouse_down(MouseButton::Left);
+        let mouse_right = window.get_mouse_down(MouseButton::Right);
+        if mouse_left || mouse_right {
             if let Some((mouse_x, mouse_y)) = window.get_mouse_pos(MouseMode::Clamp) {
-                let mouse_x = mouse_x as i32;
-                let mouse_y = mouse_y as i32;
+                let (window_width, window_height) = window.get_size();
+                let mouse_x = (mouse_x * (WIDTH as f32 / window_width as f32)) as i32;
+                let mouse_y = (mouse_y * (HEIGHT as f32 / window_height as f32)) as i32;
                 for y in mouse_y-4..mouse_y+4 {
                     for x in mouse_x-4..mouse_x+4 {
                         if x >= 0 && x < WIDTH as i32 && y >= 0 && y < HEIGHT as i32 {
-                            simulation.grid[x as usize + y as usize * WIDTH] = PixelState::Sand;
-                        }
-                    }
-                }
-            }
-        }
-        if window.get_mouse_down(MouseButton::Right) {
-            if let Some((mouse_x, mouse_y)) = window.get_mouse_pos(MouseMode::Clamp) {
-                let mouse_x = mouse_x as i32;
-                let mouse_y = mouse_y as i32;
-                for y in mouse_y-4..mouse_y+4 {
-                    for x in mouse_x-4..mouse_x+4 {
-                        if x >= 0 && x < WIDTH as i32 && y >= 0 && y < HEIGHT as i32 {
-                            simulation.grid[x as usize + y as usize * WIDTH] = PixelState::Stone;
+                            simulation.grid[x as usize + y as usize * WIDTH] = if mouse_left { PixelState::Sand } else { PixelState::Stone };
                         }
                     }
                 }
